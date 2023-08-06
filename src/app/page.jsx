1ded27b5fe035987de2149d6e466107bc01e4ad6
Md. Ashiqul Islam <ashiqul.ba@pakiza.com>
2023-08-06 17:57:01 +0600

    second update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,45 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
+import { Lottie } from "lottie-react";
+import homeBanner from "../../public/Home-banner.json";
 
 export default function Home() {
+  const homeLottie = {
+    loop: true,
+    autoplay: true,
+    animationData: homeBanner, // Your Lottie animation JSON
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
-    <div>
-      <div className={styles.main}>
-        <h1>Hello I'm XXX</h1>
-        <Image src="/hero.png" alt="home-banner" height={500} width={500} />
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better development for your Better Web Application.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <Link href="/portfolio">
+          <button className={styles.home_button}>See Our Work</button>
+        </Link>
+      </div>
+      <div className={styles.item}>
+        {/* <Image
+          className={styles.img}
+          src="/hero.png"
+          alt="home-banner"
+          height={500}
+          width={500}
+        /> */}
+        <Lottie
+          animationData={homeLottie} // Your Lottie animation JSON
+          loop
+          autoplay
+        />
       </div>
     </div>
   );
