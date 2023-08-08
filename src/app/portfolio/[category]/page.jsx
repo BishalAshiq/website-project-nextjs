b232380b01b74891163f10c5aea2fcd5bcd2228c
Md. Ashiqul Islam <ashiqul.ba@pakiza.com>
2023-08-08 11:12:02 +0600

    updated

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,24 +1,48 @@
 import React from "react";
 import PortfolioCategoriesCss from "./page.module.css";
 import Image from "next/image";
+import { items } from "./data.js";
+// import { notFound } from "next/navigation";
+
+const getData = (cat) => {
+  const data = items[cat];
+
+  if (data) {
+    return data;
+  }
+
+  return [
+    {
+      title: "Data Not Found",
+      desc: "Sorry, no data available for this category.",
+    },
+  ];
+};
 
 const Category = ({ params }) => {
-  console.log(params);
+  const data = getData(params.category);
+
   return (
     <div className={PortfolioCategoriesCss.container}>
       <h1 className={PortfolioCategoriesCss.catTitle}>{params.category}</h1>
-      <div className={PortfolioCategoriesCss.item}>
-        <div className={PortfolioCategoriesCss.content}>
-          <h1 className={PortfolioCategoriesCss.title}>Test</h1>
-          <p className={PortfolioCategoriesCss.desc}>Desc</p>
-          <button text="See More" url="#">
-            See More
-          </button>
+
+      {data.map((item) => (
+        <div className={PortfolioCategoriesCss.item} key={item.id}>
+          <div className={PortfolioCategoriesCss.content}>
+            <h1 className={PortfolioCategoriesCss.title}>{item.title}</h1>
+            <p className={PortfolioCategoriesCss.desc}>{item.desc}</p>
+            <button text="See More" url="#" />
+          </div>
+          <div className={PortfolioCategoriesCss.imgContainer}>
+            <Image
+              className={PortfolioCategoriesCss.img}
+              fill={true}
+              src={item.image}
+              alt=""
+            />
+          </div>
         </div>
-        <div className={PortfolioCategoriesCss.imgContainer}>
-          <Image src="" alt="" />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
